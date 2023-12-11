b4c680b15d9033e085acf0595e0918330d4680e8
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2023-12-11 18:47:23 +0530

    Destructuring of an array and usage of spread operator in JavaScript

--- index.js
+++ index.js
@@ -1,52 +1,16 @@
-//JavaScript Date() object
+//Destructuring of an Array
+//syntax -> const [values] = arrayName
 
-//initialize new Date object
-let d = new Date();
+const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-//using strings as an arguments in date object
-d = new Date('01/31/2000 09:30:00');
-// console.log(d);
+const [one, two, , four, ...rest] = numbers;
 
-d = new Date('01-31-2001 11:00:15');
-// console.log(d);
+console.log(one, two, four, rest);
 
-//using Numbers as an argumemts
-d = new Date(2023, 11, 9, 16, 30, 30);
-// console.log(d);
+const max = Math.max(...numbers);
+console.log(max);
 
-//TimeStamp:- number of milliseconds starting from 1st January 1970
-d = Date.now();
-// console.log(d);
+const min = Math.min(...numbers);
+console.log(min);
 
-//get the timestamp to a specific date
-d = new Date('01-31-2000 11:00:00');
-// console.log(d.getTime());
-// console.log(d.valueOf());
-
-//Usind DateTimeFormat API
-let y;
-d = new Date();
-// console.log(y);
-
-y = Intl.DateTimeFormat('default').format(d);
-// console.log(y);
-
-y = Intl.DateTimeFormat('en-GB').format(d);
-// console.log(y);
-
-y = Intl.DateTimeFormat('en-GB', {
-  day: 'numeric',
-  month: 'long',
-  year: 'numeric'
-}).format(d);
-// console.log(y);
-
-//using Locale string Method to Date Object
-y = d.toLocaleString('en-GB', {
-  weekday: 'long',
-  day: 'numeric',
-  month: 'numeric',
-  year: 'numeric'
-});
-
-console.log(y);
+console.log(...numbers);
