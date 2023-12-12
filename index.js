831656ac85841fb568637e14c2ad5dbe2ae65643
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2023-12-12 16:31:44 +0530

    Immediately Invoked function Expression (IIFE)

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-//Arrow functions in JavaScript
-// Arrow functions allows a short syntax for writing { function expression }
+//IIFE -> (Immediately Invoked function expressions)
+(function () {
+  console.log('Shubham Jaiswal');
+})();
 
-//Arrow functions with implicit return
-const sum = (num1, num2) => num1 + num2;
-
-console.log(sum(3, 4));
-
-//arrow function with explicit return
-const sum1 = (num1, num2) => {
-  return num1 + num2;
-};
-
-console.log(sum1(3, 4));
+//Adding parameters to IIFE
+(function (name) {
+  console.log(`Hello, ${name}`);
+})('John Doe');
