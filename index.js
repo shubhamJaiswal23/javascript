08eac17bafd0152fe6eb60e63388aea209065ab6
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2023-12-12 16:50:29 +0530

    returning an object using arrow function

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-function person() {
-  const firstName = 'John';
-  function name() {
-    const lastName = 'Doe';
-    console.log(`${firstName} ${lastName}`);
-  }
-  name();
-}
+//Returning an Object with arrow function
 
-person();
+//First Method
+const objReturn = () => {
+  return {
+    name: 'John Doe',
+    age: 40
+  };
+};
+
+console.log(objReturn());
+
+//Second Method
+const objReturn2 = () => ({ name: 'John Doe', age: 40 });
+
+console.log(objReturn2());
+
+//Third Method
+const objReturn3 = (name, age) => {
+  return {
+    name,
+    age
+  };
+};
+
+console.log(objReturn3('Shubham', 23));
+
+//Fourth Method
+const objReturn4 = (name, age) => ({ name, age });
+
+console.log(objReturn4('John Cena', 46));
