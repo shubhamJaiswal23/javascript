68eee2d1d8968b2e4518c63ea264a66e72951cbf
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2023-12-12 16:19:24 +0530

    functions in JavaScipt

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-//Destructuring of an Object
-//syntax -> const {key} = object;
+//functions in JavaScript
+//syntax -> function funcName (para1, para2, ...){
+// code to be executed
+//}
 
-const person = {
-  name: 'Shubham',
-  age: 23,
-  user: {
-    email: 'shubhamjaiswal@gmail.com'
-  }
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(3, 4));
+
+//In order to get the output of a function we use the { return } keyword.
+
+//function expresssion in JavaScript
+const minus = function (num1, num2) {
+  return num1 - num2;
 };
 
-const { name, age: myAge } = person;
+console.log(minus(3, 2));
 
-console.log(person);
+const minus1 = (num1, num2) => num1 - num2;
 
-console.log(name);
-
-//Assign new name to age property
-console.log(myAge + 1);
-
-const {
-  user: { email }
-} = person;
-
-console.log(email);
+console.log(minus1(4, 2));
