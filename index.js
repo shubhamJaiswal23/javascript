880499ae1179255fb7be2d14ec75e7f39301106b
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2023-12-18 17:16:38 +0530

    fetch options in Async JS

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 //Fetch options
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// function createPost(post) {
+//   fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       title: post.title,
+//       body: post.body
+//     }),
+//     headers: {
+//       'Content-Type': 'application/json',
+//       token: 'WORD@$7X'
+//     }
+//   })
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+// }
+
+// createPost({ title: 'Post-1', body: 'This is the body of Post-1' });
+
+const sendDataToServer = (post) => {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: post.title,
+      body: post.body
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      token: 'Haridwar024'
+    }
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => console.log(data));
+};
+
+sendDataToServer({ title: 'Day-11', body: 'Daily Progess Update' });
