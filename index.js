b84da055a515272f8a97113f422f89cbf78ee8e6
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2023-12-18 16:58:58 +0530

    Asynchronous JavaScript

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-//ternary operator in JavaScript
-function voterAgeValidation(age) {
-  age = +age;
-  // if (age < 18) {
-  //   alert('You are not Eligible to Vote');
-  // } else {
-  //   alert('You are Eligible to Vote');
-  // }
-  age < 18
-    ? alert('You are not Eligible to Vote.')
-    : alert('You are Eligible to Vote');
-}
-
-voterAgeValidation(prompt('What is your Age?'));
+//Fetch options
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((res) => res.json())
+  .then((data) => console.log(data));
